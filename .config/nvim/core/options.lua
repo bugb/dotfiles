a314c16371679346cb5ce165d04fe6e2229334e5
Chau Giang <admin@devops.ci>
2022-12-24 17:41:05 +0700

    neovim-keymaps: use space as leader and prefix for tab management

--- .config/nvim/core/options.lua
+++ .config/nvim/core/options.lua
@@ -1,3 +1,26 @@
+local builtin = require('telescope.builtin')
 local map = vim.api.nvim_set_keymap
 local defaults = { noremap = true, silent = true }
-map("i", "jj", "<esc>l", defaults)
+
+-- map jj to esc 
+map('i', 'jj', '<esc>l', defaults)
+
+-- map leader to <Space>
+vim.keymap.set("n", " ", "<Nop>", { silent = true, remap = false })
+vim.g.mapleader = " "
+
+-- map for telescope
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+for i=1,9,1
+do
+  vim.keymap.set('n', '<leader>'..i, i.."gt", {})
+end
+vim.keymap.set('n', '<leader>0', ":tablast<cr>", {})
+
+--Thank to the commit: https://github.com/vijaymarupudi/nvim-fzf-commands/issues/7
+--map('n', '<Leader>f', ':lua require("fzf-commands").files({command_flags="--hidden --exclude .git --exclude node_modules"})<CR>', defaults)
+
