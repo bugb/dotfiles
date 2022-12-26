ea14f8a85a7fb4667c94306f349463462ba499e5
Chau Giang <admin@devops.ci>
2022-12-26 15:56:53 +0700

    neovim-keymaps: refactor keymaps

--- .config/nvim/core/options.lua
+++ .config/nvim/core/options.lua
@@ -1,28 +1,32 @@
 local builtin = require('telescope.builtin')
-local map = vim.api.nvim_set_keymap
+local nmap = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 local set = vim.opt
 local defaults = { noremap = true, silent = true }
 
 -- map jj to esc 
-map('i', 'jj', '<esc>l', defaults)
+nmap('i', 'jj', '<esc>l', defaults)
 
 -- map leader to <Space>
-vim.keymap.set("n", " ", "<Nop>", { silent = true, remap = false })
+map("n", " ", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
 
 -- map for telescope
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+map('n', '<leader>ff', builtin.find_files, {})
+map('n', '<leader>fg', builtin.live_grep, {})
+map('n', '<leader>fb', builtin.buffers, {})
+map('n', '<leader>fh', builtin.help_tags, {})
 
 for i=1,9,1
 do
-  vim.keymap.set('n', '<leader>'..i, i.."gt", {})
+  map('n', '<leader>'..i, i.."gt", {})
 end
-vim.keymap.set('n', '<leader>0', ":tablast<cr>", {})
+map('n', '<leader>0', ":tablast<cr>", {})
 
+-- Discovered it when using vim-forgit
+-- https://github.com/ray-x/forgit.nvim/issues/1
 set.shellcmdflag = '-ic'
+
 --Thank to the commit: https://github.com/vijaymarupudi/nvim-fzf-commands/issues/7
 --map('n', '<Leader>f', ':lua require("fzf-commands").files({command_flags="--hidden --exclude .git --exclude node_modules"})<CR>', defaults)
 
